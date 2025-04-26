89366bdacd9b46b8f4423df199ca25df58674ae6
杨娇 <3273473023@qq.com>
2025-04-26 12:28:57 +0800

    Layout静态结构搭建

--- xiaotuxian_project/src/views/Layout/index.vue
+++ xiaotuxian_project/src/views/Layout/index.vue
@@ -1,13 +1,12 @@
-<template>
-  <div>
-    这是首页
-    <router-view />
-  </div>
-</template>
-
-<script>
-export default {};
+<script setup>
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
 </script>
 
-<style>
-</style>
+<template>
+  <LayoutNav />
+  <LayoutHeader />
+  <router-view></router-view>
+  <LayoutFooter />
+</template>
