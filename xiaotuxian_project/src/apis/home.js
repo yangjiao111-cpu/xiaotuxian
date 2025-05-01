3e2f97f7ea576616b39f30fd43549870d2081201
杨娇 <3273473023@qq.com>
2025-05-01 11:47:48 +0800

    home-新鲜好物和热点模块实现

--- xiaotuxian_project/src/apis/home.js
+++ xiaotuxian_project/src/apis/home.js
@@ -5,3 +5,18 @@ export function getBannerAPI() {
         url: '/home/banner'
     })
 }
+export function getNewsAPI() {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+export function getHotAPI() {
+    return httpInstance({
+        url: '/home/hot'
+    })
+}
+export function getGoodsAPI() {
+    return httpInstance({
+        url: '/home/goods'
+    })
+}
--- xiaotuxian_project/src/views/Layout/Home/index.vue
+++ xiaotuxian_project/src/views/Layout/Home/index.vue
@@ -4,6 +4,7 @@ import HomeCategory from "./components/HomeCategory.vue";
 import HomeHot from "./components/HomeHot.vue";
 import HomeNew from "./components/HomeNew.vue";
 import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
