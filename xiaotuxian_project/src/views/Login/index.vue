d9788ab0f373ec4965c849b0823060c3c2402fd5
杨娇 <3273473023@qq.com>
2025-05-03 12:40:36 +0800

    登录-表单检验-自定义检验规则

--- xiaotuxian_project/src/views/Login/index.vue
+++ xiaotuxian_project/src/views/Login/index.vue
@@ -4,6 +4,7 @@ import { ref } from "vue";
 const form = ref({
   account: "",
   password: "",
+  agree: false,
 });
 // 准备规则对象
 const rules = {
@@ -12,6 +13,12 @@ const rules = {
     { required: true, message: "密码不能为空", trigger: "blur" },
     { min: 6, max: 14, message: "密码长度为6-14个字符", trigger: "blur" },
   ],
+  agree: {
+    validator: (rule, value, callback) => {
+      if (value) callback();
+      else callback(new Error("请勾选协议"));
+    },
+  },
 };
 </script>
 
@@ -52,7 +59,7 @@ const rules = {
                 <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item prop="agree" label-width="22px">
-                <el-checkbox size="large">
+                <el-checkbox v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
