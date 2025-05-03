2d206efd2ca6cb3e731f33ca5651a2d868dc17fd
杨娇 <3273473023@qq.com>
2025-05-03 12:22:26 +0800

    登录-表单校验

--- xiaotuxian_project/src/views/Login/index.vue
+++ xiaotuxian_project/src/views/Login/index.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref } from "vue";
+// 准备表单对象
+const form = ref({
+  account: "",
+  password: "",
+});
+// 准备规则对象
+const rules = {
+  account: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+  password: [
+    { required: true, message: "密码不能为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码长度为6-14个字符", trigger: "blur" },
+  ],
+};
 </script>
 
 
@@ -31,11 +45,11 @@
               label-width="60px"
               status-icon
             >
-              <el-form-item label="账户">
-                <el-input />
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item prop="agree" label-width="22px">
                 <el-checkbox size="large">
