e2fee68c0ac633d1d1099c1b66ddb453d726a844
杨娇 <3273473023@qq.com>
2025-05-03 16:05:50 +0800

    登录-pinia数据持久化

--- xiaotuxian_project/src/main.js
+++ xiaotuxian_project/src/main.js
@@ -2,6 +2,7 @@
 import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 // 引入懒加载指令并且注册
 import { lazyPlugin } from './directives/index'
 // 引入全局组件插件
@@ -9,7 +10,10 @@ import { componentPlugin } from './components/index'
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
-app.use(createPinia())
+const pinia = createPinia()
+// 注册持久化插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
 app.use(lazyPlugin)
