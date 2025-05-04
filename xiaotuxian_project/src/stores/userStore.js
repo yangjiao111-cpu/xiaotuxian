36b0bdf2b7d322ad8afe55639b1cb790dab2f513
杨娇 <3273473023@qq.com>
2025-05-04 20:17:28 +0800

    登录时合并购物车

--- xiaotuxian_project/src/stores/userStore.js
+++ xiaotuxian_project/src/stores/userStore.js
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 import { ref } from 'vue'
 import { loginAPI } from '@/apis/user'
 import { useCartStore } from "./cartStore";
+import { mergeCartAPI } from '@/apis/cart'
 export const useUserStore = defineStore('user', () => {
     // 定义管理用户的state
     const userInfo = ref({})
@@ -11,6 +12,15 @@ export const useUserStore = defineStore('user', () => {
     const getUserInfo = async ({ account, password }) => {
         const res = await loginAPI({ account, password })
         userInfo.value = res.result
+        // 登录时将本地购物车合并到服务器
+        await mergeCartAPI(cartStore.cartList.map((item) => {
+            return {
+                skuId: item.skuId,
+                selected: item.selected,
+                count: item.count
+            }
+        }))
+        cartStore.UpdateNewList()
     }
     // 退出时清除用户信息
     const clearUserInfo = () => {
